1be1a5c01caa29c91e9cfdae3fcacd88bd8e20c3
vellen <ellen.vanmaren@gmail.com>
2023-07-12 10:05:47 +0200

    adding nmf functions

---
+++ NMF/functions/NMF_funcs.py
@@ -0,0 +1,95 @@
+import os
+import numpy as np
+import sys
+import sklearn
+from scipy import stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+from tkinter import *
+from sklearn.decomposition import NMF
+import pandas as pd
+import random
+
+def get_nnmf(X, rank, init='nndsvda',it=2000):
+    """Non-negative matrix factorization, remove zero rows before computation."""
+    W = np.zeros((X.shape[0], rank))
+    zero_rows = np.where(X.mean(axis=1) == 0)[0]
+    nonzero_rows = np.where(X.mean(axis=1) > 0)[0]
+    X0 = np.delete(X, zero_rows, 0)
+
+    model = NMF(n_components=rank, init=init, max_iter=it)
+    W0 = model.fit_transform(X0)
+    H = model.components_
+    W[nonzero_rows, :] = W0
+
+    return W, H
+
+def get_nnmf_forced(m, rank, H0, W0, it=500):
+    """Non-negative matrix factorization with initial guess, remove zero rows before computation."""
+    W = np.zeros((m.shape[0], rank))
+    zero_rows = np.where(m.mean(axis=1) == 0)[0]
+    nonzero_rows = np.where(m.mean(axis=1) > 0)[0]
+    X0 = np.delete(m, zero_rows, 0)
+    W0 = np.delete(W0, zero_rows, 0)
+    model = NMF(n_components=rank, init='custom', max_iter=it)
+    W0 = model.fit_transform(X0, H=H0, W=W0)
+    H = model.components_
+    W[nonzero_rows, :] = W0
+    return W, H
+
+def get_W_corr(Wa, Wb):
+    """Construct n by k matrix of Pearson product-moment correlation coefficients for every combination of two columns in A and B"""
+    return np.corrcoef(Wa.T, Wb.T)
+
+def max_corr(corr):
+    """Get the mean of the absolute maximum correlation coefficient for each row."""
+    return np.abs(corr).max(axis=0).mean()
+
+def amariMaxError(correlation):
+    """
+    Computes what Wu et al. (2016) described as a 'amari-type error'
+    based on average distance between factorization solutions.
+    """
+    maxCol = correlation.abs().max(axis=0)
+    colTemp = (1 - maxCol).mean()
+    maxRow = correlation.abs().max(axis=1)
+    rowTemp = (1 - maxRow).mean()
+    return (rowTemp + colTemp) / 2
+
+def stabNMF(M_input, num_it=100, k0=2, k1=10, init='nndsvda',it=2000):
+    d = M_input.shape[0]  # number of features
+    stability = np.zeros((k1 - k0 + 1,))
+    instability = np.zeros((k1 - k0 + 1,))
+
+    W_all = np.zeros((num_it, d, k1 + 1))  # preallocate W_all for all k
+
+    for k_num, k in enumerate(range(k0, k1 + 1)):  # for each rank
+        for n in range(num_it):
+            W, H = get_nnmf(M_input, k, init=init,it=it)
+            W_all[n, :, k_num] = W
+
+        for i in range(num_it):
+            for j in range(i, num_it):
+                x = W_all[i, :, k_num]
+                y = W_all[j, :, k_num]
+                CORR = get_W_corr(x, y)
+                simMat_ij = max_corr(CORR) if i != j else 0  # amariMaxError(CORR) if i == j else max_corr(CORR)
+                distMat_ij = amariMaxError(CORR)
+                stability[k_num] += simMat_ij / (num_it * (num_it - 1) / 2)
+                instability[k_num] += distMat_ij / (num_it * (num_it - 1))
+
+    return stability, instability
+
+
+# Initiation of input matrix V that should be clustered using NMF of a rank between k0 an d k1
+V = np.random.random(30,1000)
+k0 = 2
+k1 = 5
+# run stability NMF for different ranks
+_, instability = stabNMF(V, num_it=100, k0=k0, k1=k1, init='nndsvda',it=2000)
+# select rank with lowest instability value
+ranks = np.arange(k0, k1)
+rank_sel = ranks[np.argmin(instability)]
+
+# rerun NMF with chosen best rank
+W, H = get_nnmf(V, rank_sel, init='nndsvda',it=2000)
--- NMF/main.py
+++ NMF/main.py
@@ -5,15 +5,10 @@ import h5py
 import numpy as np
 import sys
 from sklearn.decomposition import NMF
+sys.path.append('./functions/')
+import NMF_funcs
 
-# Replace this with your own NMF function
-def apply_nmf(data, n_components=5):
-    model = NMF(n_components=n_components, init='random', random_state=0)
-    W = model.fit_transform(data)
-    H = model.components_
-    return W, H
-
-def main(inputfolder, stab_NMF, k):
+def main(inputfolder, stab_NMF, k0, k1):
     # Create output folder
     output_folder = os.path.join(inputfolder, "NMF_output")
     os.makedirs(output_folder, exist_ok=True)
@@ -22,14 +17,37 @@ def main(inputfolder, stab_NMF, k):
     for filename in os.listdir(inputfolder):
         if filename.endswith(".csv") and filename.startswith("EL"):
             # Load data
-            tab = pd.read_csv(os.path.join(inputfolder, filename))
-            print(filename)
-            data = np.random.rand(10,200)
+            # Load data
+            con_trial = pd.read_csv(os.path.join(inputfolder, filename))
+            con_trial = con_trial[con_trial.Artefact < 2]
+            con_trial = con_trial.reset_index(drop=True)
+            ## 1. Add unique connection label for each StimxChan combination: Con_ID
+            con_trial['Con_ID'] = con_trial.groupby(['Stim', 'Chan']).ngroup()
+            ## normalize LL based on the mean of LL_pre per Chan
+            con_trial['LL_onset_norm'] = con_trial.groupby('Chan').apply(lambda x: x['LL_onset'] / x['LL_pre'].mean()).reset_index(0, drop=True)
+            ## fill nan with mean of specifc Con_ID
+            con_trial['LL_onset_norm'].fillna(con_trial.groupby('Con_ID')['LL_onset_norm'].transform('mean'), inplace=True)
+            df_pivot = con_trial.pivot(index='Con_ID', columns='Block', values='LL_onset_norm')
+            # If there are still missing values after pivot, you might want to fill them with the global mean
+            df_pivot.fillna(con_trial['LL_onset_norm'].mean(), inplace=True)
+            V = df_pivot.values
+
             # Apply NMF
             if stab_NMF:
-                W, H = apply_nmf(data, n_components=k)
+                print('running stability NMF')
+                # run stability NMF for different ranks
+                _, instability = NMF_funcs.stabNMF(V, num_it=100, k0=k0, k1=k1, init='nndsvda',it=2000)
+                # select rank with lowest instability value
+                ranks = np.arange(k0, k1)
+                k = ranks[np.argmin(instability)]
+
+                # rerun NMF with chosen best rank
+                print('running NMF with a chosen rank of '+str(k))
+                W, H = NMF_funcs.get_nnmf(V, k, init='nndsvda',it=2000)
             else:
-                W, H = apply_nmf(data, n_components=k)
+                k = k0
+                print('running NMF with a rank of '+str(k))
+                W, H = NMF_funcs.get_nnmf(V, n_components=k)
             # Save output
             output_filename = filename.replace(".csv", "_nmf.h5")
             with h5py.File(os.path.join(output_folder, output_filename), 'w') as hf:
@@ -37,13 +55,23 @@ def main(inputfolder, stab_NMF, k):
                 hf.create_dataset('H', data=H)
                 hf.create_dataset('k', data=k)
 
+            # Assigning the clusters to each connection
+            clusters = np.argmax(W, axis=1)
+
+            # Create a dictionary with connection_id as keys and cluster as values
+            cluster_dict = {i: cluster for i, cluster in enumerate(clusters)}
+            # Add the 'Cluster' column to the DataFrame
+            con_trial['Cluster'] = con_trial['Con_ID'].map(cluster_dict)
+            con_trial.to_csv(os.path.join(output_folder, filename.replace(".csv", "_cluster.csv")), header=True, index=False)
+
     print('---DONE -----')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process CSV files and apply NMF.')
     parser.add_argument('inputfolder', type=str, help='Input folder path')
     parser.add_argument('--stab_NMF', type=int, default=0, help='Binary flag for stability NMF')
-    parser.add_argument('--k', type=int, default=5, help='Number of clusters for NMF')
+    parser.add_argument('--k0', type=int, default=2, help='Number of clusters for NMF')
+    parser.add_argument('--k1', type=int, default=10, help='Number of clusters for NMF')
     args = parser.parse_args()
 
-    main(args.inputfolder, args.stab_NMF, args.k)
+    main(args.inputfolder, args.stab_NMF, args.k0, args.k1)
