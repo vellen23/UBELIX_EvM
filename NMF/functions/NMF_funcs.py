78bd4ce14189bb6b8220a3edc4f905f17c8a878e
vellen <ellen.vanmaren@gmail.com>
2023-07-12 11:28:33 +0200

    Update NMF_funcs.py

--- NMF/functions/NMF_funcs.py
+++ NMF/functions/NMF_funcs.py
@@ -58,18 +58,18 @@ def stabNMF(M_input, num_it=100, k0=2, k1=10, init='nndsvda',it=2000):
     stability = np.zeros((k1 - k0 + 1,))
     instability = np.zeros((k1 - k0 + 1,))
 
-    W_all = np.zeros((num_it, d, k1 + 1))  # preallocate W_all for all k
-
     for k_num, k in enumerate(range(k0, k1 + 1)):  # for each rank
+        # for each rank value
+        W_all = np.zeros((num_it, d, k))
         for n in range(num_it):
-            W, H = get_nnmf(M_input, k, init=init,it=it)
-            W_all[n, :, k_num] = W
+            W, H = get_nnmf(M_input, k)
+            W_all[n, :, :] = W
 
         for i in range(num_it):
             for j in range(i, num_it):
-                x = W_all[i, :, k_num]
-                y = W_all[j, :, k_num]
-                CORR = get_W_corr(x, y)
+                x = W_all[i]
+                y = W_all[j]
+                CORR = get_BF_corr(x, y)
                 simMat_ij = max_corr(CORR) if i != j else 0  # amariMaxError(CORR) if i == j else max_corr(CORR)
                 distMat_ij = amariMaxError(CORR)
                 stability[k_num] += simMat_ij / (num_it * (num_it - 1) / 2)
