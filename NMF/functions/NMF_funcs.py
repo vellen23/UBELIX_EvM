2db4a10cdb93a6bd507734a95987140fc3f27de1
van Maren <ellen.vanmaren@artorg.unibe.ch>
2023-08-07 14:36:23 +0200

    add req_conda

--- NMF/functions/NMF_funcs.py
+++ NMF/functions/NMF_funcs.py
@@ -6,8 +6,77 @@ from scipy import stats
 from sklearn.decomposition import NMF
 import pandas as pd
 import random
+import staNMF as st
+from staNMF.nmf_models import spams_nmf
+from scipy.stats import entropy
 
-def get_nnmf(X, rank, init='nndsvda',it=2000):
+
+# This function will compute the entropy for each row of the input matrix
+def get_entropy(L):
+    E = np.array([entropy(row) for row in L])
+    A = entropy(E)
+    B = np.mean(E)
+    return E, A, B
+
+
+def recursive_stanmf(X, k_range, max_clusters, clusters=None, idx=None, parent_entropy=None, cluster_label='',
+                     threshold=0.1, level=0):
+    if idx is None:
+        idx = np.arange(X.shape[0])
+
+    if clusters is None:
+        clusters = []
+
+    # Stop if the number of data points is less than the smallest possible number of clusters or if maximum number of clusters has been reached
+    if X.shape[0] < min(k_range) or len(clusters) >= max_clusters:
+        clusters.append((cluster_label, X, idx))
+        return clusters
+
+    # Apply stability NMF and compute instability
+    folderID = "your_folder_"
+    model = st.staNMF(X, folderID=folderID, K1=min(k_range), K2=max(k_range), replicates=20, seed=123)
+    model.NMF_finished = True
+    model.runNMF(spams_nmf(bootstrap=False))
+    model.instability("spams_nmf")
+    best_k = k_range[np.argmin(model.get_instability())]
+
+    # Run NMF with best k
+    model = NMF(n_components=best_k, init='random', random_state=0)
+    W = model.fit_transform(X)
+
+    best_f_values = []
+    new_clusters = []  # New list to keep track of the new clusters
+    for i in range(best_k):
+        # todo: find better way for cluster assignment
+        component_idx = np.argmax(W, axis=1) == i
+        best_clusters = X[component_idx]
+        new_label = cluster_label + 'X' + str(level + 1) + str(i + 1)  # Append the child label to the parent label
+
+        _, A_cluster, B_cluster = get_entropy(best_clusters)
+        f_value = A_cluster - B_cluster
+
+        if parent_entropy is None or f_value < parent_entropy:
+            best_f_values.append(f_value)
+            new_clusters.append((new_label, best_clusters, idx[component_idx]))
+
+    # Only go to next level of recursion if the conditions based on f and g values are met
+    for new_cluster in new_clusters:
+        new_label, best_clusters, new_idx = new_cluster
+
+        clusters = recursive_stanmf(best_clusters, k_range, max_clusters, clusters, new_idx, f_value, new_label,
+                                    threshold, level + 1)
+
+        if len(best_f_values) == 0:  # New condition to handle empty sub_f_values
+            clusters.append(new_cluster)
+        else:
+            g_value = sum(abs(np.array(best_f_values) - np.array(best_f_values)))
+            if g_value > threshold:
+                clusters.append(new_cluster)
+
+    return clusters
+
+
+def get_nnmf(X, rank, init='nndsvda', it=2000):
     """Non-negative matrix factorization, remove zero rows before computation."""
     W = np.zeros((X.shape[0], rank))
     zero_rows = np.where(X.mean(axis=1) == 0)[0]
@@ -20,6 +89,7 @@ def get_nnmf(X, rank, init='nndsvda',it=2000):
     W[nonzero_rows, :] = W0
 
     return W, H
+
 
 def get_nnmf_forced(m, rank, H0, W0, it=500):
     """Non-negative matrix factorization with initial guess, remove zero rows before computation."""
@@ -34,9 +104,11 @@ def get_nnmf_forced(m, rank, H0, W0, it=500):
     W[nonzero_rows, :] = W0
     return W, H
 
+
 def get_W_corr(Wa, Wb):
     """Construct n by k matrix of Pearson product-moment correlation coefficients for every combination of two columns in A and B"""
     return np.corrcoef(Wa.T, Wb.T)
+
 
 def max_corr(corr):
     """Get the mean of the absolute maximum correlation coefficient for each row."""
@@ -55,14 +127,13 @@ def amariMaxError(correlation):
     return (rowTemp + colTemp) / 2
 
 
-
-def stabNMF(M_input, num_it=100, k0=2, k1=10, init='nndsvda',it=2000):
+def stabNMF(M_input, num_it=100, k0=2, k1=10, init='nndsvda', it=2000):
     d = M_input.shape[0]  # number of features
     stability = np.zeros((k1 - k0 + 1,))
     instability = np.zeros((k1 - k0 + 1,))
 
     for k_num, k in enumerate(range(k0, k1 + 1)):  # for each rank
-        print(str(k_num)+'/'+str(k1 - k0 + 1), end="\r")
+        print(str(k_num) + '/' + str(k1 - k0 + 1), end="\r")
         # for each rank value
         W_all = np.zeros((num_it, d, k))
         for n in range(num_it):
