e43195fbae802234a53978fb5d181f81dcd1d9bd
vellen23 <ellen.vanmaren@gmail.com>
2023-08-28 18:35:36 +0200

    venv update

--- job_BM.sh
+++ job_BM.sh
@@ -2,17 +2,21 @@
 #SBATCH --job-name=parallel_nmf
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=2  # Adjust this based on your cluster's resources
-#SBATCH --mem=16GB  # Adjust this based on your cluster's resources
+#SBATCH --mem=64GB  # Adjust this based on your cluster's resources
 
 # Load any necessary modules
 module load Anaconda3
-
+module list
 # Activate virtual environment if needed
 # conda activate your_environment
+cd /storage/homefs/ev19z018/Code/
 source venv/bin/activate
+which python
+echo "Activated venv"
+
 
 # go to Folder with python scripts
-cd ./NMF/
+cd ./UBELIX_EvM/NMF/
 
 # Run the Python script
 python3 main_BM.py --inputfolder=/storage/homefs/ev19z018/Data/BM_CR/ --parallel=1
